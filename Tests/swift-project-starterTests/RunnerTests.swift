c9dd07fb6e0a35a062572745b1dde60f49e85f4f
swiftty <62803132+swiftty@users.noreply.github.com>
2026-04-21 03:21:20 +0900

    Add default-config command test

--- Tests/swift-project-starterTests/RunnerTests.swift
+++ Tests/swift-project-starterTests/RunnerTests.swift
@@ -3,37 +3,44 @@ import Testing
 @testable import swift_project_starter
 
 struct `swift-project-starterTests` {
-    private func exec(_ subcommand: String, _ arguments: String...) throws -> (
-        stdout: String,
-        stderr: String
-    ) {
-        try exec(subcommand, arguments)
-    }
 
-    private func exec(_ subcommand: String, _ arguments: [String]) throws -> (
-        stdout: String,
-        stderr: String
-    ) {
-        let setting = try #require(DirectoryScope.current)
-
-        let bin = productsDirectory.appending(path: "swift-project-starter")
-
-        let stdout = Pipe()
-        let stderr = Pipe()
-        let process = try shell(
-            bin, ["init"] + arguments,
-            currentDirectoryURL: setting.workingDirectory,
-            stdout: stdout, stderr: stderr,
-        )
-
-        process.waitUntilExit()
-
-        func string(_ pipe: Pipe) throws -> String {
-            let data = try pipe.fileHandleForReading.readToEnd() ?? Data()
-            return String(data: data, encoding: .utf8) ?? ""
+    @Test(
+        .directoryScope(name: "Example", to: fixturesDirectory),
+        .setupSwiftPackage,
+    )
+    func `test default-config command`() throws {
+        // swift-format-ignore
+        let expected = """
+        {
+          "dependencies" : [
+            {
+              "from" : "1.0.0",
+              "url" : "https:\\/\\/github.com\\/swiftty\\/swift-format-plugin"
+            }
+          ],
+          "project" : {
+            "resources" : [
+              {
+                "content" : "{\\n  \\"indentation\\": {\\n    \\"spaces\\": 4\\n  },\\n  \\"lineLength\\": 120,\\n  \\"multilineTrailingCommaBehavior\\": \\"alwaysUsed\\",\\n  \\"multiElementCollectionTrailingCommas\\": false,\\n  \\"rules\\": {\\n    \\"NoAccessLevelOnExtensionDeclaration\\": false\\n  },\\n  \\"version\\": 1\\n}\\n",
+                "filePath" : ".swift-format"
+              }
+            ],
+            "type" : "library"
+          },
+          "swiftSettings" : [
+            {
+              "enableUpcomingFeature" : "InternalImportsByDefault"
+            },
+            {
+              "enableUpcomingFeature" : "NonisolatedNonsendingByDefault"
+            }
+          ]
         }
 
-        return (try string(stdout), try string(stderr))
+        """
+        let (output, error) = try exec("default-config", "--package-path", ".", "--project", "library")
+        #expect(output == expected)
+        #expect(error == "")
     }
 
     @Test(
@@ -122,5 +129,40 @@ struct `swift-project-starterTests` {
         #expect(content(of: ".swift-format") != "")
         #expect(content(of: "Makefile") != "")
         #expect(content(of: "project.yml") != "")
+    }
+}
+
+extension `swift-project-starterTests` {
+    private func exec(_ subcommand: String, _ arguments: String...) throws -> (
+        stdout: String,
+        stderr: String
+    ) {
+        try exec(subcommand, arguments)
+    }
+
+    private func exec(_ subcommand: String, _ arguments: [String]) throws -> (
+        stdout: String,
+        stderr: String
+    ) {
+        let setting = try #require(DirectoryScope.current)
+
+        let bin = productsDirectory.appending(path: "swift-project-starter")
+
+        let stdout = Pipe()
+        let stderr = Pipe()
+        let process = try shell(
+            bin, [subcommand] + arguments,
+            currentDirectoryURL: setting.workingDirectory,
+            stdout: stdout, stderr: stderr,
+        )
+
+        process.waitUntilExit()
+
+        func string(_ pipe: Pipe) throws -> String {
+            let data = try pipe.fileHandleForReading.readToEnd() ?? Data()
+            return String(data: data, encoding: .utf8) ?? ""
+        }
+
+        return (try string(stdout), try string(stderr))
     }
 }
